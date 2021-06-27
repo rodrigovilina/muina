99e42127672a047d6c6f1c8bdb175381b7319b0b
vaporyhumo <roanvilina@gmail.com>
2021-06-27 21:50:54 +0000

    Remove signatures from `Muina::PrivateCreation`

--- lib/muina/private_creation.rb
+++ lib/muina/private_creation.rb
@@ -4,7 +4,6 @@
 module Muina
   # Include this module to make `.allocate` and `.new` private
   module PrivateCreation
-    T::Sig::WithoutRuntime.sig { params(klass: Module).void }
     def self.included(klass)
       klass.private_class_method :allocate
       klass.private_class_method :new
