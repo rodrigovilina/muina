86322be8a715428cecccb224342e127e00f26d92
vaporyhumo <roanvilina@gmail.com>
2021-06-27 21:51:20 +0000

    Remove signatures from `Muina::Result`

--- lib/muina/result.rb
+++ lib/muina/result.rb
@@ -12,7 +12,6 @@ module Muina
     const :value, T.untyped
     const :error, T.untyped
 
-    T::Sig::WithoutRuntime.sig { params(success_klass: T.untyped, error_klass: T.untyped).returns(T.untyped) }
     def self.[](success_klass, error_klass) # rubocop:disable Metrics/MethodLength
       Class.new(self) do
         const :value, T.any(success_klass, Unit), override: true
@@ -28,12 +27,10 @@ module Muina
       end
     end
 
-    T::Sig::WithoutRuntime.sig { params(value: T.untyped).returns(T.attached_class) }
     def self.success(value)
       new(value: value, error: Unit.instance)
     end
 
-    T::Sig::WithoutRuntime.sig { params(error: T.untyped).returns(T.attached_class) }
     def self.failure(error)
       new(value: Unit.instance, error: error)
     end
