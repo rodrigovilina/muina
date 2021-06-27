e4f855040db57e39a237786747c135aa7089113b
vaporyhumo <roanvilina@gmail.com>
2021-06-27 21:50:30 +0000

    Remove signatures from `Muina::Params`

--- lib/muina/params.rb
+++ lib/muina/params.rb
@@ -7,12 +7,6 @@ module Muina
     include T::Props
     include T::Props::Constructor
 
-    # :nocov:
-    T::Sig::WithoutRuntime.sig do
-      params(params: T.any(T::Hash[T.untyped, T.untyped], ActionController::Parameters))
-        .returns(T.attached_class)
-    end
-    # :nocov:
     def self.extract(params)
       params = ActionController::Parameters.new(params) if params.instance_of?(Hash)
       TypedParams[self].new.extract!(params)
