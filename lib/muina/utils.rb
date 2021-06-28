240cfc10e50eff1a0f0d52c9f8032c044c4ddd20
vaporyhumo <roanvilina@gmail.com>
2021-06-28 21:09:08 +0000

    Refactor utils

--- lib/muina/utils.rb
+++ lib/muina/utils.rb
@@ -4,14 +4,12 @@
 module Muina
   # Casting utils
   module Utils
-    T::Sig::WithoutRuntime.sig { params(errors: T.untyped).returns(T::Array[T.untyped]) }
     def self.cast_to_errors(*errors)
       errors
-        .flat_map { |error| error.instance_of?(T::Types::Union) ? cast_union_to_errors(error) : error }
-        .map { |error| cast_to_error(error) }.uniq
+        .flat_map { |error| flatten_union(error) }
+        .map      { |error| cast_to_error(error) }.uniq
     end
 
-    T::Sig::WithoutRuntime.sig { params(type: BasicObject).returns(T.untyped) }
     def self.cast_to_error(type) # rubocop:disable Metrics/MethodLength
       case type
       when T::Types::Simple then type.raw_type
@@ -21,12 +19,10 @@ module Muina
       end
     end
 
-    T::Sig::WithoutRuntime.sig { params(error: T::Types::Union).returns(T::Array[T.untyped]) }
     def self.cast_union_to_errors(error)
       error.types.map { |type| cast_to_error(type) }
     end
 
-    T::Sig::WithoutRuntime.sig { params(failure_klass: Classes).returns(Module) }
     def self.errors_rescue_module(failure_klass) # rubocop:disable Metrics/MethodLength
       failures = cast_to_errors(failure_klass)
 
@@ -34,6 +30,13 @@ module Muina
         klass.define_singleton_method(:===) do |exception|
           [*@failures].any? { |failure| exception.instance_of?(failure) }
         end
+      end
+    end
+
+    def self.flatten_union(error)
+      case error
+      when T::Types::Union then cast_union_to_errors(error)
+      else error
       end
     end
   end
